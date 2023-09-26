cc11263facc46f24d4651f31d54e57e38c09b7c8
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-09-26 15:10:41 -0400

    Update Contact API Fix

--- API/UpdateContact.php
+++ API/UpdateContact.php
@@ -1,29 +1,31 @@
 <?php
     $input = json_decode(file_get_contents('php://input', true));
+    $firstname = $input['firstname'];
+    $lastname = $input['lastname'];
+    $email = $input['email'];
+    $phoneNumber = $input['phone'];
+    $contactID = $input['contactID'];
+    
     $conn = new mysqli("localhost", "nstuh", "COP4331Contact", "COP4331");
 
     if($conn->$connect_error){
         returnWithError($conn->connect_error);
+    }else{
+        $sqlUpdate = "UPDATE Contacts SET FirstName = '$firstname' , LastName = '$lastname', EmailAddress = '$email', PhoneNumber = '$phoneNumber' WHERE ID = '$contactID'";
+	
+
+        if(($sqlQuery = $conn->query($sqlUpdate)) === TRUE)
+        {
+            $returnString = '{"name":"' . $name . '","phone":"' . $phoneNumber . '","email":"' . $email . '"}';
+            echo $returnString;
+        }
+        else 
+        {
+            echo "Error: Did not update";
+        }
+
+	$conn -> close();
+
     }
-
-    $stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, PhoneNumber = ?, EmailAddress = ? WHERE ID = ?");
-    $stmt->bind_param("sssss", $input["firstname"], $input["lastname"], $input["phone"], $input["email"], $input["contactID"]);
-    $stmt->execute();
-    
-    $stmt->close();
-    $conn->close();
-    
-
-    function returnWithInfo( $id )
-	{
-		$retValue = '{"id":"' . $id . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-
-    function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
     
 ?>
