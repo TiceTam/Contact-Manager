b1bc7a544dcc7c2265cc4b61e984e9dbf0b99a9a
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-09-26 15:03:19 -0400

    Update Contact fix

--- API/UpdateContact.php
+++ API/UpdateContact.php
@@ -1,16 +1,14 @@
 <?php
-    $input = json_decode(file_get_contents("php://input", true));
+    $input = json_decode(file_get_contents('php://input', true));
     $conn = new mysqli("localhost", "nstuh", "COP4331Contact", "COP4331");
 
     if($conn->$connect_error){
         returnWithError($conn->connect_error);
     }
 
-    $stmt = $conn->prepare("UPDATE Contacts SET (FirstName = ?, LastName = ?, PhoneNumber = ?, EmailAddress = ?) WHERE ID = ?");
+    $stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, PhoneNumber = ?, EmailAddress = ? WHERE ID = ?");
     $stmt->bind_param("sssss", $input["firstname"], $input["lastname"], $input["phone"], $input["email"], $input["contactID"]);
     $stmt->execute();
-
-    returnWithInfo($input["contactID"]);
     
     $stmt->close();
     $conn->close();
