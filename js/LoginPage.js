4883cf0066f64e53f181bd5f64ac789546962821
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-09-27 16:41:36 -0400

    Fetch login API

---
+++ js/LoginPage.js
@@ -0,0 +1,31 @@
+let loginButton = document.getElementById("loginButton");
+
+
+loginButton.addEventListener('click',()=>{
+    let username = document.getElementById("typeEmail").value;
+    let password = document.getElementById("typePassword").value;
+
+    let currentUser = {
+        "username": username,
+        "password": password
+    }
+
+    fetch("API/Login.php",{
+        "method": "GET",
+        "headers": {
+            "Content-Type" : "application/json; charset=utf-8"
+        },
+
+        "body" : JSON.stringify(currentUser)
+
+        })
+        .then(function(response){
+          console.log("it worked?");
+        return response.JSON();
+        
+        })
+        .then(function(data){
+          console.log(data);
+        });
+
+});
