a95c54e185e8f673baf2d296cd5d5f740b30c34c
Isaac Tam <123422802+TiceTam@users.noreply.github.com>
2023-10-03 16:22:27 -0400

    Create LoadContacts.js
    basic set up for load contacts, displays in table still need to work on table/listing

---
+++ js/LoadContacts.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    let userId = 1; // Replace with the actual user ID
+
+    const contactsContainer = document.getElementById("contactsContainer"); // replace with your actual container ID.
+
+    function loadContactsForUser(userId) {
+        // send an API request to fetch contacts for the user
+        fetch(`API/LoadContacts.php?userID=${userId}`, {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.results.length > 0) {
+                    contactsContainer.innerHTML = '';
+
+                    // iterate through the contacts and display them in the container
+                    data.results.forEach((contact) => {
+                        displayContact(contact);
+                    });
+                } else {
+                    // no contacts found
+                    contactsContainer.innerHTML = '<p>No contacts found.</p>';
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                // errors during request
+                contactsContainer.innerHTML = '<p>Error occurred while loading contacts.</p>';
+            });
+    }
+
+    // display a contact in the container
+    function displayContact(contact) {
+        // creates new HTML element for the contact (div, list, table, etc)
+        const contactElement = document.createElement("div");
+        contactElement.classList.add("contact");
+        contactElement.innerHTML = `
+            <p>Name: ${contact.firstname} ${contact.lastname}</p>
+            <p>Email: ${contact.email}</p>
+        `;
+        // can add more to this div
+
+        // Append the contact element to the container
+        contactsContainer.appendChild(contactElement);
+    }
+
+    // Load contacts for the current user when the page loads
+    loadContactsForUser(userId);
+});
