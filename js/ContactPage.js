983824b34c3590f1df88e02daa717c7329c6b706
belthomsen <belthomsen@gmail.com>
2023-10-06 03:56:12 -0400

    Update ContactPage.js for modal

--- js/ContactPage.js
+++ js/ContactPage.js
@@ -242,7 +242,7 @@ function load(info){
         c3.innerText = contacts[i-1].lastname
         c4.innerText = contacts[i-1].email
         c5.innerText = contacts[i-1].phone
-        c6.innerHTML = "<button id ='editButton' onClick='editContact()'>Edit</button><a href='../contact_page.html' class='btn btn-info btn-lg' role='button' data-toggle='modal' data-target='#deleteContact'>Delete Contact</a>"
+        c6.innerHTML = "<button id ='editButton' onClick='editContact()'>Edit</button><a href='../contact_page#deleteContact' class='btn btn-info' role='button' data-toggle='modal'>Delete Contact</a>"
         // "<button id = 'deleteButton' onClick= 'deleteContact()'>Delete</button>";
     }
 
